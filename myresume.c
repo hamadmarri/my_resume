3524f7eb578a87d467c496b584479d9de444e779
Hamad Marri <hamad.s.almarri@gmail.com>
2020-01-15 09:27:01 +1100

    first commit

---
+++ myresume.c
@@ -0,0 +1,234 @@
+/*
+    This is an obfuscated C source code that prints my resume.
+    To see my resume you need to run this code
+    To compile and run (in linux): gcc myresume.c && ./a.out | less
+    If you are a windows user who hires developers (please don't hire me).
+    If you are an HR who doesn't know what this is, consult your programmers (they are much more useful than you).
+
+    HSA
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define _H_ []
+#define _D_ =
+#define _S_ char
+#define _E_ int
+#define _Y_ if
+#define _X_ else
+#define _Z_ printf
+#define _$_ ()
+#define _U_ ;
+#define _A_ _S_ _ _H_ _D_
+#define _L_ _S_ __ _H_ _D_
+#define _M_ _S_ ___ _H_ _D_
+#define _AA_ 0x5F
+#define _V_ for
+#define _R_ _S_ ____ _H_ _D_
+#define _RR_ _S_ _____ _H_ _D_
+#define _I_ _S_ ______ _H_ _D_
+#define _T_ continue
+#define retern_size(X) _Z_("%c", X)
+
+_E_ _______[_AA_];
+
+void _____ _$_ {
+    _E_ ______ _U_ _V_ (______ _D_ 0x4F - 0x20
+    _U_ ______ < 0x7F - 0x20 + 0x2F
+    _U_ ______++) _______[______ - 0x4F + 0x20]
+    _D_ ______ % (0x7F - 0x20) + 0x20;
+}
+
+void _P_(_S_ _ _H_) {
+    _E_ __;
+    _S_ ___ _U_ _V_ (__ _D_ 0
+    _U_ __ < strlen(_) - 1
+    _U_ __ += 2) { _Y_ (_[__ + 1] == '\r') { __++;
+    _T_; } _X_ _Y_ (_[__] == '\r') { _T_;
+    } ___ _D_ _[__]; _[__] _D_ _[__ + 1];
+    _[__ + 1] _D_ ___; }
+}
+
+void getsize(_S_ ______) {
+    _Y_ (______ == '\r' || ______ == '\n') retern_size(______);
+    _X_ _Y_ (______ > 0x5E) retern_size(_______[______ - _AA_]);
+    _X_ retern_size(_______[______]);
+}
+
+void srtlen(_S_ _ _H_) { 
+    _E_ __ _U_ _V_ (__ _D_ 0
+    _U_ __ < strlen(_)
+    _U_ __++) getsize(_[__]);
+}
+
+void ____ _$_ {
+    _A_ "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+                    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\r\n\r\n\r\n";
+    _L_ "0000000000000000000000000000000000=<====================<=\r\n"
+                    "00000000000000000000000000000000000-QXQ]0TQcY\\0]\\Q]0bQYb-0\r\n"
+                    "0000000000000000000000000000000000=7====================7=\r\n\r\n\r\n";
+    _M_ 
+    "B0=BFBS0# y#q}0|%c0< g||~  ww~0<c^<gQ0$&#%|qqy0<EB@@-0@0BD=CHE=HFB0C0-qxq}>t>$|qq}##Py}wyq>| s}"
+    "\r\n\r\n\r\n\r\n";
+    _P_(_);
+    _P_(__);
+    _P_(___);
+    srtlen(_);
+    srtlen(__);
+    srtlen(___);
+}
+
+void ___ _$_ {
+    _R_ "==================<=\r\nc0V_gdbQ0U^UYWU^bU-0\r\n==================7=\r\n";
+    _RR_ 
+        "T0tusy%qtuq0t~x0wy|x0*$q&%u%c0v (%#q0u~Uywu~#u(0%y0x)uu%$~'y0u)uu!y#~uusy00~#!w q#}}~y0w~q0tqtq%\r\n"
+        "}0~qwq}u~u>%V0}q|yqy0#y(x%'0#q y$&!0 ##w}qy}w~%0  $|0<rqu|%00 #%& |r$u x% q0t~q0q~*|u+$0v (%#q0ux(|yu\r\n"
+        "t0%uyqy|w~v0~yytw~0$~y!0 #uv$$ yq~0|~q0t ss~$y0uu# !%#>$`0 #yvys~u0%~yq0!!y|qsy%~ t0'u|u! u}%~q0t~!0 ##w}qy}w~\r\n"
+        "q00$u(||q00$uty$~wq0t~y0!}u|u}%~%q y>~`0$ u$$$$uq0{0uu0~%qu%%~ y0~ %t0%uyq0|x%%qy00$u(||}0yq%~yqu~0t~&ut#\r\n"
+        "!0u#$$#&>uQ0$|0 )uu!y#~uus0t~y%0qu0}q}q~uwu}%~q0t~w0%uy%w~%0ux}0$ 0%& 0%v u0!} |u*$u>\r\n\r\n\r\n";
+    _P_(____);
+    _P_(_____);
+    srtlen(____);
+    srtlen(_____);
+}
+
+void ______ _$_ {
+    _R_ "==================<=\r\nS0b_0U_S`]dU^UYScU-0\r\n==================7=\r\n";
+    _RR_ 
+        "0: c%vq(u#c0$*u%$}T0$uwy0~00000000000:wQ|y0u c%vq(u#T0'u|u! u}%~0000000000000:y]#ss } &!u%0#*c%$}u0$uTy$~w\r\n"
+        "0: S!}%&#uc0$*u%$}Q0s#yxu%%s#&$u00000:qTq%]0~y~y0w06q`%%#u0~ub s~w%y y0~00000:qTq%qru$d0  $|\r\n"
+        "0:#Qy%yvys|qY0%~|uy|uws~0u00000000000: c%vq(u#e0y~0%06~Yu%#w%q y0~ud%$~y0w000:qTq%]0~ %y# ~yw\r\n"
+        "0:qTq%\\0w 0$06ub !%#0$000000000000000:!Q|!sy%q y0~#`w q#}}~y0w000000000000000:ud}q]0~qwq}u~u%\r\n"
+        "0:Ye60e00h#`w q#}}~y0w000000000000000:|Suy%~60c0#uu'0##`w q#}}~y0w00000000000: c%vq(u#Q0q~*|y$$\r\n\r\n"
+        "c0v (%#q0u06udxsy~qs0|#`v syuys~uyJ$S0S?;;-0Z0'q0q0-!cy#w~]0Sf-0Z0Vc-0Z0Q`-0]0c*\\a-0]0~  wRT-0\r\n"
+        "W0q|$$yvx$-0d0} qs0%0-X`0`0-dX\\]0E0-cS0c0-qzq's$y#%!-0Q0w~|&#qcZ-0z0&a#u0*0- T z-0^0t zu0$-\r\n"
+        "Q0t~ #tyc0&%yt0 0-#s$ ($|q0{#!z su0%0-!_~u$0usu~w0q#x!-0 0u!s~0'0-xc|u0|scy#%!~y0w0-Rf^>dU-0\r\n"
+        "Q0`c^>dU-0c0#s}&-0h00`0-uT%!0xyv$#0%u$#qxs-0R0u#tqx%V0#y%$c0qus#0x0-uR%$V0#y%$c0qus#0x-\r\n"
+        "Q0]bC00ByR0% S%#)u]=0C Su#-0c0]dBCC0=Byr0%bQ0] S%#)u-0S0~ #%| $0$*u%$}-0`0S\\s0~ #%| $0$*u%}\r\n\r\n\r\n";
+    _P_(____);
+    _P_(_____);
+    srtlen(____);
+    srtlen(_____);
+}
+
+void __ _$_ {
+    _R_ "==========<\r\n`0_bUZdS0c-\r\n==========7\r\n";
+    _RR_ 
+        "0:q[q$%y0J0Q$&#uv0y#~u|t0*!q0! v0#y|%$~u~y0w %q0q#yr0s&}y$>s[0$qyq0%$yr0y&%|y00~hee?0Y#t'y~uq0!! #sqx\r\n"
+        "00x(#u0ux%0u~yu%v#sq$uq0u#t0q#~(q0t~t0sutytur0vu# 0u%$#qy%w~y0%~0  sut>\r\n"
+        "00000= Ww u|`0q|J*x0%%$!?J!?q|>* ww u|s>} $? %u#q?!!?$utq%|yO$tysM} x>}qtq{>$qyq%\r\n\r\n"
+        "0:uc *q~0Juc *q~(0|y0|u|0% *0&qxu'q0'0ty us0|q0|y(x%%0ux 0u~(0 x|0  ${|0{y0u *0& }%$0>%Y%0{q$u%0uxv0squ\r\n"
+        "00uv%q#&$uq0t~t00 q}xs~y0uu|#qy~w~!0 #us$$ 00~|q0|qvus0$ %}0%qxs(0%y0xu!s#~uq%uw$0qsu|q0||v0sq$u%0w %uux>#\r\n"
+        "00vQu%0# *0&q%u{q0$0|u0vy!%s#&0u v0# *#&v0sq<uc0*u~ 0qy(||v0~y0tx%0uur%$}0%qxs(0uxu#%0ux!0#u $0~x%%q*0& q0u#\r\n"
+        "00 w~y0w %s0|q0|qx0$x%0u }%$v0sq0uy$y}q|y#y%$u(0%y0x *0&}q~ 0w|q0|~ y|u~&0u$$#0>v_s0& $#<uy00vux 00#x$0u$y~0% \r\n"
+        "00&r*$q00%x%0u }u}%~s0|qy|w~ 0x%#u!0 u|!>ui0& s0~qu0u'0~x$#q0u *#&!0sy&%u#0$&ty#w~%0uxs0|q>|\r\n"
+        "00000=ug$r%yJux0%%J!??u$ *q~s>} \r\n"
+        "00000= Ww u|`0q|J*x0%%$!?J!?q|>* ww u|s>} $? %u#q?!!?$utq%|yO$tysM} x>}qtq}>q*!!y|qsy%~ \r\n\r\n"
+        "0:yWX%r&0J%x!%J$??ywx%r&s>} x?}qtqq}##Oyq%Mru# !y$ %y#$u\r\n\r\n\r\n";
+    _P_(____);
+    _P_(_____);
+    srtlen(____);
+    srtlen(_____);
+}
+
+void ________ _$_ {
+    _R_ "==========<\r\nb0cUQUSb0X-\r\n==========7\r\n";
+    _RR_ 
+        "0:!Q|!y*w~`0%q0x S$~$yu%s~0*|q wy#x%0}~ \"0q&y|q%y%u's0~ %$q#~y0%u~(%# 0{a8^S09~y 0t##u%00 u#&tus%0ux\r\n"
+        "00&~r}#u 00v&\"#uuy>$a0u&y#$uq0u# 0t##utux0&uy#%$sy|q*|%00 ~uqxs~0uq`x%S0~ y$%$~u*s%00 |u}y~y%qu\r\n"
+        "00~y s$~$yu%%~!0y#y}y%u'>$d0ux#0$uqus#0xq!u!0#\\2qu~#~y0w&a|q%y%q'y0u S$~#%yq%~^0%u ({#2$x0$qr0uu~\r\n"
+        "00squs%!tur00*x%0uEBx%Y0%~#uq~y%~ |qc0}* !y$}& 00~ud!}# |qb0!uu#u$%~%q y0~~q0tub$q~ ~y0wd8]Y0U@BHA>9\r\n"
+        "00&Qx%# J$]0|q{u]0&  xr&0<qXq}0t|Q]0#qy#q0t~U0$y0q|Q~q+q>y\r\n"
+        "00000=%x!%J$??y%u}@BHAy>y!q!>~q(>(|!!? ##w}qq?ss!uu%=tq!u!$#\r\n"
+        "00000=%x!%J$??#t! >$qt$w&%|xt>?u! $&\'?| %|)uu%B?A@?HGIDH!?vt\\?`YsY=$YdU]B=A@=HIA!>vt\r\n"
+        "00000=%x!%J$??#t! >$qt$w&%|xt>?u! $&\'?| %|)uu%B?A@?HHIDD!?vt|?!ysy=$ \'A|@B%=}yBuA@=H s!}u|u%!>vt\r\n\r\n\r\n";
+    _P_(____);
+    _P_(_____);
+    srtlen(____);
+    srtlen(_____);
+}
+
+void _ _$_ {
+    _R_ "====================<=\r\nS0bQUU0bSQYXfU]U^Ucd-0\r\n====================7=\r\n";
+    _RR_ 
+        "0:#`w q#}}tuq0c0c]~0% vysy%q y0~&vs~y%~ y0%~0  }y~ %y#w~$0v (%#q<uq0u|%#~y0w~qq0!! #u'0tv yvys|qy0v\r\n"
+        "00u#tq~y$w(0#u0u)uustu~y0wus%#yq0~q!q#u}u%$#>\r\n\r\n"
+        "0:xS$ ~u%00 #%'q|u%00 x%0u[eq0t~!0 #y'utv0uurtsq0{~q0t~q|q$*$y 00~\\^di<U!0 #y'ytw~q0\r\n"
+        "00u# s}}~uqty%~ %0qx0%u#&$%|tuy00~x%0u&!s#qxu$ 00vx%0u $%vq(u#r00*x%0u s!}~q>*\r\n\r\n\r\n";
+    _P_(____);
+    _P_(_____);
+    srtlen(____);
+    srtlen(_____);
+}
+
+void __________ _$_ {
+    _R_ "================<=\r\ng0b_0[hUU`Yb^UUS-0\r\n================7=\r\n";
+    _RR_ 
+        "0:qTq%S0~uu%0#!_#u%q# -0c0RQSY=0Z0r&yq<|c0&qytQ0q#yr0q00000000000000000000000000q]0*@BG@=0V0ru0>@B@A\r\n"
+        "00000=ub!$~ y$|r0u v0# }y~ %y#w~%0uxt0%q0qus%~#u0=~y|st&~y0wu$'##u<$!0( #u$0!&|!uy0$~q0tyq0# st~%y yy~w~\r\n"
+        "000000~&%y=$$0uu~y0w %%0uxx0qu%|0x~q0tu#y|rq|y%y0*v %0ux$0$*u%>}\r\n"
+        "00000=q]~yq%~ytuq0$0vq0u ({#|!sq<u 0u'$#uu~y0wx%0u#!! #u!0 # % s0|~q0t&wty|u~y$u(0#u0uu}0%~y#0|u%q y0~ %\r\n"
+        "000000}u|!* uux0qu%|0x~q0tq$uv*%$0q%t~#q$t>\r\n"
+        "00000= ^y%yvtu$0$*u%0}tqy}y~%$q# %$# 00v~q0*$y&$$u(0%y0xx%0uqtq%s0~uu%<#t0%uyqy|w~!0 #|r}u0$~ys0u|#qq0t~s0~ ysu$\r\n"
+        "000000u# !%#0$~y|st&~y0w|q0|u#u|q'%~y0v~# q}y%~ >\r\n"
+        "00000=&cu!'#$ytu%0ux%0 #r&u|x$  y%w~q0t~#0!uyq0#v %0uxt0%q0qus%~#u0<q}y{w~$0#&0u#%& |r$u x% ~y0wty~uy%yvtuq0||%0ux\r\n"
+        "000000u#u|q'%~y0$$u&0$~q0t|q0|u#q!#y0$u(u#s0#qy#tu 0%&!0 #u!|#>*\r\n"
+        "00000=u`v## u}0tx!$*sy|qs0ux{s<$}0{q~y0w&$u#%0uxu#(0#u0u ~x0+q#q$t 00#% ux0#u~qvy#& 0$|u}u~u$%%0qx0%y}xw%\r\n"
+        "000000~yu%v##u0uy(x%t0%q0qus%~#u!0 #us$$$u>\r\n\r\n"
+        "0:#`w q#}}#u-0j0}q|yU0u~w#0*uc'#sy$uS0} q!*~=0Q00|x[r #q0<qct&0y#Qrqqy0000000000uV>rB0@@0F0=q]0*@BG@\r\n"
+        "00000=uTu' |u!0t^UbU_c0\\ $%vq(u#%00  }y~ %0#|q0||usu#%sy|q!0#q}q%u#u0$v |0#quwu0u~w#0*|!~q$%q0t~v0sq %y#$u>\r\n"
+        "00000=uTy$~wtu%0ux$0v (%#q0u %x0'q0uu# !%#0$}uyqu|0t~q0t#%~qv$#uu#0tx% #w&0x]c0c&q %q}y%qs||0*~qt\r\n"
+        "000000 s}}~&sy%q0uy(x%t0wy%y|q}0%u#u0$x% #w&0xQ\\0^~q0t~yu%~#%u0<#!' ty~y0w }u#s0~ u'y~~u0%squs$$q0t~&0q$uw>\r\n"
+        "00000=#`w q#}}tuq00~}u#uuws~0*|q#u0%*$%$}uv0# }0r |y0ux!~ $uy00~x%0u'u~u0%v q00~~&$&q&0|u#tq~y<w}0{q~yw\r\n"
+        "000000u#!$~ u$%0}y0u&\"syu{0#~q0t}y#!' ~y0wx%0u' #u|q0|q$uv*% 00vx%0u*$%$}u>\r\n"
+        "00000=~Q|q+*tu%0uxy0!~%&0$#v} %0uxt0wy%y|qu0u~w#0*u}u%0#~q0tq}!!tu%0 xu$!0#q}q%u#u0$ %r00u s}}~&sy%qtuq0t~\r\n"
+        "000000yt!$q|u*0tx% #w&0xx%0u $%vq(u#>\r\n"
+        "00000=#`w q#}}tuq0t~s0$& %y}u+0tx%0uyt!$q|0*q!q#u}u%$#y00~x%0u v}# 00vq%&rq|0#qtq%q0t~%0u#t~<$w0q#x!~y0wx%u\r\n"
+        "000000~y v}#%q y0~ v0#qu*$&0t~#u%$~qytw~>\r\n\r\n\r\n";
+    _P_(____);
+    _P_(_____);
+    srtlen(____);
+    srtlen(_____);
+}
+
+void _________ _$_ {
+    _R_ "============================<\r\nU0eTQSYd^_60S0bUYdYVQSYd^_0c-\r\n============================7\r\n";
+    _RR_ 
+        "0:%ct&0* v0#0qq]%$#u$7t0wuu#0u~yS0} &!u%0#scuys~0u000000000000000000000000000000000000@BIA=0S0#&u#%~\r\n"
+        "00000= S!}%&#uq0t~^0%u ({#~y0wuc&sy#*%\r\n\r\n"
+        "0:%ct&0* v0#0qq]%$#u$7t0wuu#0u~yS0} &!u%0#scuys~0u000000000000000000000000B0A@0F0=@BHA80#d~qv$#uu#9t\r\n"
+        "00~e'y#uy$*% 00vubywq~=0b0wu~y<qc0[\r\n"
+        "00000=xd$u$yy00~#Qy%yvys|qY0%~|uy|uws~u\r\n"
+        "00000=q`x%S0~ y$%$~u*sq0t~T0} yq0~ S$~#%yq%~0$\"Qy&u#\r\n"
+        "00000=#d~qv$#uu#0t %e0y~u'$#%y0*v g0|  |w~~ w\r\n\r\n"
+        "0:>R>Qscc0v (%#q0u*c%$}u0$~Uywu~#u~y0w0-~e'y#uy$*% 00vubywq~=0b0wu~y<qc00[0000000000000000000000@BEA\r\n"
+        "00000=uT~q$7\\0$y0%~Uywu~#u~y0wV8|q0|@BBA9\r\n"
+        "00000=qV&s%|0*v U0w~~yuuy#w~q0t~Q0!!y|tuc0ys~uusT0qu7~0$u]qt0|B8A@9E\r\n\r\n"
+        "0:y]#s$ v 0%uS%#vyuy0t!Q|!sy%q y0~uTu' |u!0#0000000000000000000000000000000000000000000000000000@BH@\r\n\r\n"
+        "0:y]#s$ v 0%uS%#vyuy0t#`v $uy$~ |q00000000000000000000000000000000000000000000000000000000000000@BE@\r\n\r\n"
+        "0: S!}%&#u`0 ##w}qy}w~60Q0!!y|qsy%~ 0$yT|!} 0q00000000000000000000000000000000000000000000000000@BEA\r\n"
+        "00|QQ0q|y}xqY0$~y%&%u%=0T0}qq}<}c0&qytQ0q#yrq\r\n\r\n\r\n";
+    _P_(____);
+    _P_(_____);
+    srtlen(____);
+    srtlen(_____);
+}
+
+void ___________ _$_ {
+    _I_ "0000000000000000000000000000000000=<================================================<\r\n"
+                "00000000000000000000000000000000000-{c|y$|q0u#w0| $t0<&}%$~0% r00uqxt~tu%00 ty y$%0>-\r\n"
+                "0000000000000000000000000000000000=7================================================7\r\n\r\n\r\n";
+    _A_ "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\r\n\r\n\r\n";
+    _P_(______);
+    _P_(_);
+    srtlen(______);
+    srtlen(_);
+}
+
+int main(int argc, char const *argv[])
+{
+    _____ _$_;____ _$_;___ _$_;______ _$_;__ _$_;________ _$_;
+    _ _$_;__________ _$_;_________ _$_;___________ _$_;
+    return 0;
+}
