cedc4600e94ec9753433e2bb07856e9475db2ddd
Hamad Marri <hamad.s.almarri@gmail.com>
2020-01-16 06:59:03 +1100

    docs

--- README.md
+++ README.md
@@ -3,8 +3,4 @@
     To see my resume you need to run this code
     
     To compile and run (in linux): gcc myresume.c && ./a.out | less
-    
-    If you are a windows user who hires developers (please don't hire me).
-    
-    If you are an HR who doesn't know what this is, consult 
-    your programmers (they are much more useful than you).
+
--- myresume.c
+++ myresume.c
@@ -2,8 +2,6 @@
     This is an obfuscated C source code that prints my resume.
     To see my resume you need to run this code
     To compile and run (in linux): gcc myresume.c && ./a.out | less
-    If you are a windows user who hires developers (please don't hire me).
-    If you are an HR who doesn't know what this is, consult your programmers (they are much more useful than you).
 
     HSA
 */
